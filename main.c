b087bfcd283f59d587fa1e8a6603b77718fdb3f4
asdzx5812 <weiwei272000@gmail.com>
2020-04-27 16:32:28 +0800

    update PSJF

--- main.c
+++ main.c
@@ -84,11 +84,15 @@ int main(){
 	const int shareSize = sizeof(int);
 	int segmentId = shmget(IPC_PRIVATE, shareSize, S_IRUSR | S_IWUSR);
 	end_flag = (int*)shmat(segmentId, NULL, 0);
-	
+	*end_flag = 0;
 	//sort it, then we can arrive the process in order
 	sort(Process_name, R, T, N);
 	if(flag_for_policy == 2 || flag_for_policy == 3)
 		sort_SJF(Process_name, R, T, N);
+	
+	for(i = 0; i < N; i++){
+	fprintf(stderr, "%s %d %d\n", Process_name[i], R[i], T[i]);
+		}
 	//pid is for fork()
 	//num_process_done counts how many processes had been terminated
 	//num_process_arrive counts how many processes had arrived
@@ -98,7 +102,7 @@ int main(){
 	//main_clock is the clock of main process
 	//cure_process_clock is used to check RR quantum time
 	int main_clock = 0, cur_process_clock = 0; 
-	
+
 	//begin to schedule
 	while(num_process_done < N){
 
@@ -139,10 +143,12 @@ int main(){
 					fprintf(stderr, "set process %d affinity to CPU 0 failed", num_process_arrive);
 					return -1;
 				}
-				if(num_process_arrive == 0)
-					if(sched_setparam(pid, &parammax) == -1)
+				if(num_process_arrive == 0){
+					if(sched_setparam(pid, &parammax) == -1){
 						fprintf(stderr, "set param error\n");
-
+						return -1;
+					}
+				}
 				num_process_arrive++;
 			}			
 		}
@@ -151,8 +157,6 @@ int main(){
 		//in order to deal with RR (need add arrive process in ready queue
 		//then switch process)
 		if(main_clock != 0 && !Queue_is_empty(list)){
-			cur_process_clock++;
-			list->head->remain_time--;
 			//the running process should be terminate
 			if(list->head->remain_time == 0 && (flag_for_policy == 0 || flag_for_policy == 1)){ 
 				deQueue(list);
@@ -163,10 +167,11 @@ int main(){
 				//wait until the child process get the information(terminate information)
 				while(*end_flag == 1); 
 			} 
-			else if(list->head->remain_time == 0 && flag_for_policy == 2){
+			else if(list->head->remain_time == 0){
 				deQueue(list);
 				if(!Queue_is_empty(list)){
 					findshortest(list);
+					fprintf(stderr,"%d\n", list->head->pid);
 					if(sched_setparam(list->head->pid, &parammax) == -1)
 						fprintf(stderr, "set param error\n");
 				}
@@ -188,11 +193,30 @@ int main(){
 					fprintf(stderr, "set param error\n");
 				//local ready queue update
 				mvHead(list);
-			}	
+			}
+			else if(flag_for_policy == 3){
+				if(!Queue_is_empty(list)){
+					struct Node* tmp = list->head;	
+					findshortest(list);
+					if(list->head != tmp){
+						if(sched_setparam(list->head->pid, &parammax) == -1)
+							fprintf(stderr, "set param error\n");
+						if(sched_setparam(tmp->pid, &param) == -1)
+							fprintf(stderr, "set param error\n");
+						cur_process_clock = 0;
+					}
+				}
+			}
 		}
 		//a unit time
 		wait_a_unit();
 		main_clock++;
+		fprintf(stderr, "mainclock : %d\n", main_clock);	
+		if(!Queue_is_empty(list)){
+			cur_process_clock++;
+			list->head->remain_time--;
+		fprintf(stderr, "pid  : %d remain : %d\n", list->head->pid, list->head->remain_time);	
+		}
 	}
 	//wait all child process exit, avoid zombie processes
 	while(wait(NULL) > 0);
