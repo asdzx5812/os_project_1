1c4f42e2c9818b70ccc4546c3faa7173f2b3c057
asdzx5812 <weiwei272000@gmail.com>
2020-04-28 20:08:33 +0800

    systemcall

--- kernel_file/my_time.c
+++ kernel_file/my_time.c
@@ -1,7 +1,7 @@
 #include<linux/linkage.h>
 #include<linux/kernel.h>
 #include<linux/timer.h>
-
+//systemcall 335
 asmlinkage long sys_my_time(void){
 	static const long nano = 1000000000;
 	struct timespec t;
